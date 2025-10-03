631b6043d8c6e563de5fd376007459717b4c9c81
Himanshu Dalai <senorhimanshu@gmail.com>
2025-10-03 18:06:48 +0530

    modified app in flask_app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import mlflow
 import dagshub
-from preprocessing_utility import normalize_text
+from flask_app.preprocessing_utility import normalize_text
 import pickle
 import os
 import pandas as pd
